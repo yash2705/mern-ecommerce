c3eaefaad3308731777771623c94aa7e3f3edce5
yash2705 <62518145+yash2705@users.noreply.github.com>
2022-11-30 23:31:42 +0530

    Delete frontend/redux directory

--- frontend/redux/constants/cartConstants.js
+++
@@ -1,4 +0,0 @@
-export const ADD_TO_CART = 'ADD_TO_CART'
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
-export const CART_RESET = 'CART_RESET'
-export const FETCH_MY_CART = 'FETCH_MY_CART'
--- frontend/redux/constants/userContants.js
+++
@@ -1,3 +0,0 @@
-export const SET_USER = 'SET_USER'
-export const SET_INITIAL_STATE = 'SET_INITIAL_STATE'
-export const USER_RESET = 'USER_RESET'
